3dd1bc0b6819698e07bb0b3a765efa009e310ca3
Aleksandr Gidenko <aleksgidenko@gmail.com>
2013-03-10 14:54:58 -0400

    added problem 1

---
+++ cpp/problem1.cpp
@@ -0,0 +1,22 @@
+/*
+** Project Euler
+** Problem 1
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int sum = 0;
+
+    for (int num=1; num<1000; num++) {
+        if ((num%3)==0 || (num%5)==0)
+            sum += num;
+    }
+    
+    cout << sum << endl;
+
+    return 0;
+}
---
+++ python/problem1.py
@@ -0,0 +1,9 @@
+# Project Euler
+# Problem 1
+
+sum = 0
+for num in range(1, 1000):
+    if (num % 3) == 0 or (num % 5) == 0:
+        sum += num
+
+print sum
